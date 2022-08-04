8a169b89a342698745ca13b91985c5b3edd940cf
Anas <archid-@student.1337.ma>
2022-08-04 15:55:53 +0100

    Initial commit

---
+++ tts.el
@@ -0,0 +1,49 @@
+;;; tts.el ---- Thirteen Thirthy-Seven Emacs header
+;;
+;; Filename: tts.el
+;; Description: file header for prog-mode
+;; Author: 0x0584 (mlyarchid)
+;; Created: <2022-08-04 Thu 16:07:00>
+;; Updated: <2018-08-04 Sun 16:44:06>
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;;
+;;	  Alternative to Forthy-Two file header
+;;
+;;; Summary:
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
+
+;;
+;;                                                 ▄██
+;; file:                                          ▀███   ██▀▀█▄   ██▀▀█▄ █▄▄▄▄▄▄█▀
+;; author:                                          ██  ███  ▀██ ███  ▀███     █▀
+;;                                                  ██       ▄██      ▄██     █▀
+;; created:                                         ██     ▀▀██▄    ▀▀██▄    █▀
+;; updated:                                       ▄████▄      ██       ██   █▀
+;;                                                     ███  ▄█▀ ███  ▄█▀  █▀
+;;                                                      █████▀   █████▀  █▀
+
+(provide 'tts)
+;;; tts.el ends here
